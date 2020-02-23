2c0b4b05aa396201b9dbbb1ec3cfddbac6f08196
Andras <andras@andrasq.com>
2020-02-23 15:01:07 -0500

    do not bind to user-provided uri object; 0.9.2

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function httpRequest( uri, body, callback ) {
     body = (body != undefined) ? body : (uri.body != undefined) ? uri.body : undefined;
     if (!uri || !callback) throw new Error("uri and callback required");
 
+    var noReqEnd = uri.noReqEnd, noResListen = uri.noResListen;
     var requestOptions = { headers: {} };
     if (typeof uri === 'object') {
         for (var k in uri) if (k !== 'url' && k !== 'body' && k !== 'headers' && k !== 'noReqEnd' && k !== 'noResListen') requestOptions[k] = uri[k];
@@ -39,19 +40,19 @@ function httpRequest( uri, body, callback ) {
     }
 
     var urlParts, url = (typeof uri === 'string') ? uri : uri.url;
-    // TODO: copy pathmame too for older qnit mockHttp(); nodejs ignores it
+    // copy pathmame too for older qnit mockHttp(); nodejs ignores it
     if (url != undefined) for (var k in (urlParts = Url.parse(url), { protocol:1, hostname:1, port:1, path:1, pathname:1 })) {
         if (urlParts[k] != null) requestOptions[k] = urlParts[k];
     }
 
     body = (typeof body === 'string' || Buffer.isBuffer(body)) ? body : JSON.stringify(body);
-    if (uri.noReqEnd) requestOptions.headers['Transfer-Encoding'] = 'chunked';
+    if (noReqEnd) requestOptions.headers['Transfer-Encoding'] = 'chunked';
     else requestOptions.headers['Content-Length'] = (typeof body === 'string') ? Buffer.byteLength(body) : body ? body.length : 0;
 
     var httpCaller = requestOptions.protocol === 'https:' ? https : http;
     var returned = 0, callbackOnce = function( err, res, body ) { if (!returned++) callback(err, res, body) };
     var req = httpCaller.request(requestOptions, function(res) {
-        if (uri.noResListen) return callbackOnce(null, res);
+        if (noResListen) return callbackOnce(null, res);
         var chunks = [];
         res.on('data', function(chunk) {
             chunks.push(chunk);
@@ -68,7 +69,7 @@ function httpRequest( uri, body, callback ) {
     })
 
     if (body !== undefined) req.write(body);
-    if (!uri.noReqEnd) req.end();
+    if (!noReqEnd) req.end();
 
     return req;
 }
